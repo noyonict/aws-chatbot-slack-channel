72ccfab875b78b04a5b9901acd1b6dbd22d9c431
noyon <mohaymenul.islam@m2amedia.tv>
2022-04-13 17:01:12 +0600

    filepath change for module

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 data "local_file" "cloudformation_template" {
-  filename = "./cloudformation.yml"
+  filename = file("${path.module}/cloudformation.yml")
 }
 
 resource "aws_cloudformation_stack" "chatbot_slack_configuration" {
